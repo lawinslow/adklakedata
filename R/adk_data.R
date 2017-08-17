5ff6e8eb616a2f5d645b8eb83e742b9f9418d164
Tobi Hahn <rainshapes@gmail.com>
2017-08-17 16:25:47 -0400

    documentation, match args for get_metadata and adk_data, increment version

--- R/adk_data.R
+++ R/adk_data.R
@@ -1,21 +1,38 @@
+filenames = c("chem" = "waterchemALTM1994_2012.csv",
+              "crustacean" = "crustacean_withbiomass.csv",
+              "meta" = "lake.typeandInfo.csv",
+              "nutrient" = "nutrient.csv",
+              "phyto" = "phyto_withbiomass.csv",
+              "rotifer" = "rotifer_withbiomass.csv",
+              "secchi" = "secchi.csv",
+              "tempdo" = "temp_do_profiles.csv",
+              "zoop_biomass_conversion_table" = "zoop_biomass_conversion_table.csv")
+
 #' @title Load ADK Data
 #' 
 #' 
 #' @description 
-#' Loads data from locally downloaded CSV files. Run check_dl_data before using this function.
+#' Loads data from locally downloaded CSV files. Run \code{\link{check_dl_data}} before using this function.
 #' 
-#' @param data_name A string choosing the data to load. Can be "chem", "rotifer", "phyto", "crustacean", "tempdo", or "meta".
+#' @param data_name A string choosing the data to load. 
+#' \tabular{ll}{
+#' \strong{data_name} \tab \strong{data file loaded} \cr
+#' chem \tab waterchemALTM1994_2012.csv \cr
+#' crustacean \tab crustacean_withbiomass.csv \cr
+#' meta \tab lake.typeandInfo.csv \cr
+#' nutrient \tab nutrient.csv \cr
+#' phyto \tab phyto_withbiomass.csv \cr
+#' rotifer \tab rotifer_withbiomass.csv \cr
+#' secchi \tab secchi.csv \cr
+#' tempdo \tab temp_do_profiles.csv \cr
+#' zoop_biomass_conversion_table \tab zoop_biomass_conversion_table.csv
+#' }
 #' 
 #' @export
-
 adk_data = function(data_name){
   data_name = tolower(data_name)
+  data_name = match.arg(data_name, names(filenames))
   path = local_path()
-  filenames = c("chem" = "waterchem_cleaned.csv",
-                "rotifer" = "rotifer_cleaned.csv",
-                "phyto" = "phyto_cleaned.csv",
-                "crustacean" = "crustacean_cleaned.csv",
-                "tempdo" = "TempDOprofiles_all.csv",
-                "meta" = "lake.typeandInfo.csv")
+  check_dl_file(system.file("extdata/master.csv", package = "adklakedata"), fname = filenames[[data_name]])
   return(read.csv(file.path(local_path(), filenames[[data_name]])))
 }
